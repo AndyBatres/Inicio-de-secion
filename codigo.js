8ffa7c66d50c383a95bf9f880c99f88bfc4ffabd
AndyBatres <andy.batres04@gmail.com>
2026-08-26 11:37:41 -0600

    Creacion de registro de  usuarios

--- codigo.js
+++ codigo.js
@@ -1,15 +1,24 @@
-function IniciarSesion(){
-let usuario = document.getElementById("user").value;
-let contrasena = document.getElementById("contrasena").value;
-if (usuario === "admin" && contrasena == "1234"){
-    document.getElementById("mensaje").textContent = "Inicio de sesión correcto"
-
-} else{
-    document.getElementById("mensaje").textContent = "Usuario y/o contraseña incorrecto"
+function IniciarSesion() {
+    let usuario = document.getElementById("user").value;
+    let contrasena = document.getElementById("contrasena").value;
+    if (usuario === "admin" && contrasena == "1234") {
+        document.getElementById("mensaje").textContent = "Inicio de sesión correcto"
+    } else if (usuario == "alumno" && contrasena == "4321") {
+        document.getElementById("mensaje").textContent = "Inicio de sesión correcto"
+    } else {
+        document.getElementById("mensaje").textContent = "Usuario y/o contraseña incorrecto"
+    }
 }
-}
-function reg(){
+function reg() {
     let usu = document.getElementById("usuario").value;
     let cont = document.getElementById("cont").value;
-    if(usu != null && cont != null)
+    if (usu != "" && cont != "") {
+        document.getElementById("text").textContent = "Datos guardados"
+    } else if(usu == "" && cont == ""){
+        document.getElementById("text").textContent = "Llene todas las casillas por fabor"
+    } else if(usu != "" && cont == ""){
+        document.getElementById("text").textContent = "Llene el espacio de contraseña"
+    } else if(usu == "" && cont != ""){
+        document.getElementById("text").textContent = "Llene el espacio de usuario"
+    }
 }
